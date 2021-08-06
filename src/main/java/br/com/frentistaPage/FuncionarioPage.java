94c89d9bdfb81b17a32ae5b23aeea55be11a9dff
Jairo Almeida <jairoam85@gmail.com>
2021-08-06 17:04:34 -0300

    Foi criado uma classe para interagir com a tela onde será colocado o código do veiculo, essa classe é manipulada por um funcionario

--- src/main/java/br/com/frentistaPage/FuncionarioPage.java
+++ src/main/java/br/com/frentistaPage/FuncionarioPage.java
@@ -1,18 +1,19 @@
 package br.com.frentistaPage;
 
 import org.openqa.selenium.By;
-
 import br.com.jairo.page.BasePage;
 
-public class FuncionarioPage extends BasePage {
+public class FuncionarioPage extends BasePage {	
 	
-	
-	public void abastecimentoPendentes(){
+	public void btnAbastecimentoPendentes(){
 		clicarBotao(By.xpath("//button[@class='btn btn-bold btn-pure btn-secondary']"));
 	}
-	
-	public void buscarAbastecimento(String abastecimento){
-		escrever("buscar_abastecimento", abastecimento);
+		
+	public void codigo_ID_Veiculo(String abastecimento){
+		escrevendo_E_Apertando_Enter("buscar_abastecimento", abastecimento);
 	}
-
+	
+	public void teste(){
+		obterValorCombo("id_veiculo");
+	}
 }
