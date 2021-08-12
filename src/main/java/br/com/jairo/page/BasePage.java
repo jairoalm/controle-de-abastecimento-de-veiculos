ea7bf4cece6f8074f0f545d8c0761bbc58316b69
Jairo Almeida <jairoam85@gmail.com>
2021-08-11 21:51:50 -0300

    Adiconado um metodo para escrever e apertar tecla enter

--- src/main/java/br/com/jairo/page/BasePage.java
+++ src/main/java/br/com/jairo/page/BasePage.java
@@ -2,11 +2,17 @@ package br.com.jairo.page;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import br.com.jairo.config.DriverFactory;
+
+
 
 
 public class BasePage {
 	
+	//***** Escrever no campo *****//
 	public void escrever(By by, String texto){
 		DriverFactory.getDriver().findElement(by).sendKeys(texto);	
 	}
@@ -14,13 +20,25 @@ public class BasePage {
 		escrever(By.id(id),(texto));
 	}
 	
+	//***** Botão *****//
 	public void clicarBotao(By by){
 		DriverFactory.getDriver().findElement(by).click();
 	}
 	public void clicarBotao(String id){
 		clicarBotao(By.id(id));
 	}
+	//***** Escrevendo no input e apertando a tecla ENTER *****//
+	public void escrevendo_E_Apertando_Enter(String id, String valor){
+		DriverFactory.getDriver().findElement(By.id(id)).sendKeys(valor + Keys.ENTER);		
+	}
 	
+	public String obterValorCombo(String id) {
+		WebElement element = DriverFactory.getDriver().findElement(By.id(id));
+		Select combo = new Select(element);
+		return combo.getFirstSelectedOption().getText();
+	}
+	
+	//***** Obter mensagem de texto *****//
 	public String getObetrTexto(By by){
 		return DriverFactory.getDriver().findElement(by).getText();
 		
@@ -30,6 +48,7 @@ public class BasePage {
 		return getObetrTexto(By.id(id));
 	}
 	
+	//****** Alert *****//
 	public String getMensagemAlert(){
 		Alert alert = DriverFactory.getDriver().switchTo().alert();
 		return alert.getText();
